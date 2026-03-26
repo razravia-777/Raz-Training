f1baf78844947ba6aa3e8652bf243464d1221fa8
Raz <razravia2007@gmail.com>
2026-03-26 13:10:16 +0200

    final flood fill

--- Basic-python/flood_fill_final/fill.py
+++ Basic-python/flood_fill_final/fill.py
@@ -1,6 +1,8 @@
 import argparse
 from algorithm import read_pbm_file, write_pbm_file
 from format import check_args
+import os
+from PIL import Image
 
 
 def main():
@@ -16,8 +18,12 @@ def main():
     y = args.y
 
     if check_args(input_file, output_file, x, y):  # בדיקת תקינות של הקלט מהמשתמש
-        picture_data = read_pbm_file(input_file)   # הוצאת הDATA מהקובץ
+        picture_data = read_pbm_file(input_file)  # הוצאת הDATA מהקובץ
         write_pbm_file(output_file, picture_data, x, y)  # צביעה של החלקים הרצויים
+        with open (output_file, "r") as f:
+            print(f.read())
+        img = Image.open(output_file)
+        img.show()
 
 
 if __name__ == '__main__':
